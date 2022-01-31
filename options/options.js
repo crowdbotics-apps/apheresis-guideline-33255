3c0d501cf46bd0cce445f0fd94a65bac6672fb45
Crowdbotics <team@crowdbotics.com>
2022-01-31 02:39:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "apheresis_guideline_33255",
+  url: "https://apheresis_guideline_33255.botics.co",
+  api: "https://apheresis_guideline_33255.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
